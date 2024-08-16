6e2f4928114c8e43d6cbf026589238b9d143a453
Lailson19 <contatolailsonandesson@gmail.com>
2024-08-16 04:19:06 -0300

    Atualiza layout e informações

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-const data=new Date,ano=data.getFullYear();let p=document.getElementById("ass").innerText+=" "+ano;
+const data = new Date(),
+  ano = data.getFullYear();
+let p = (document.getElementById("ass").innerText += " " + ano);
